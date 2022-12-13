43e4909a7be98968be9a7d1dcb5a0f9780daa287
Fatema Tuj Johora <tujjohoraa@gmail.com>
2022-12-14 02:52:06 +0600

    Initial commit

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import {
+  useGsapGalleryTitle,
+  useGsapGalleryCategory,
+  useGsapGalleryImage,
+} from "../hooks/gsap";
+
+const GalleryItem = ({ image }) => {
+  const galleryTitleRef = useRef(null);
+  const galleryCategoryRef = useRef(null);
+  const galleryimageRef = useRef(null);
+
+  useGsapGalleryTitle(galleryTitleRef, galleryimageRef);
+  useGsapGalleryCategory(galleryCategoryRef, galleryimageRef);
+  useGsapGalleryImage(galleryimageRef);
+
+  return (
+    <div className="gallery-item">
+      <h1 className="gallery-item-title" ref={galleryTitleRef}>
+        {image.title}
+      </h1>
+      <p className="gallery-item-category" ref={galleryCategoryRef}>
+        {image.category}
+      </p>
+      <div
+        ref={galleryimageRef}
+        className="gallery-item-img"
+        style={{ backgroundImage: `url(${image.src})` }}
+      ></div>
+    </div>
+  );
+};
+
+export default GalleryItem;
